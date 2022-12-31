2a805de54dd7cf6aae0b7e4e7db5f8f9efb7af10
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2022-12-31 08:50:46 -0800

    Create installstuff.sh

---
+++ installstuff.sh
@@ -0,0 +1,4 @@
+wget https://apt.pop-os.org/ubuntu/pool/universe/v/virt-manager/virt-manager_4.0.0-1_all.deb
+wget https://apt.pop-os.org/ubuntu/pool/universe/v/virt-manager/virtinst_4.0.0-1_all.deb
+sudo nala update
+sudo nala install ./virt-manager_4.0.0-1_all.deb ./virtinst_4.0.0-1_all.deb
