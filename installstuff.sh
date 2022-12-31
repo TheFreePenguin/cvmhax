ed576e5dde261081737f599ec6df9c4570d12157
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2022-12-31 08:59:04 -0800

    Update installstuff.sh

--- installstuff.sh
+++ installstuff.sh
@@ -1,3 +1,4 @@
+sudo apt install wget nala
 wget https://apt-origin.pop-os.org/ubuntu/pool/universe/g/gnome-boxes/gnome-boxes_42.0.1-1ubuntu1_amd64.deb
 sudo nala update
 sudo nala install ./gnome-boxes_42.0.1-1ubuntu1_amd64.deb
